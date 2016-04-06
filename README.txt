ead39687ddb69308f3b23fc09dde5f8468023f8b
swati gupta <sgupta1505@gmail.com>
2016-04-06 13:03:41 -0700

    Modified DemoGit.java to output my name with a hello message

---
+++ README.txt
@@ -0,0 +1,15 @@
+project folder:
+guptaNswati-project01
+
+
+Brief description of submitted files:
+
+src/DemoGit.java
+    - Include main.
+    - Outputs a hello message with my name.
+
+resources/RUN.txt
+    - console output of DemoGit.java
+
+README.txt
+    - description of submitted files
--- src/DemoGit.java
+++ src/DemoGit.java
@@ -1,11 +1,7 @@
+
 /**
  * Simple class to test submitting to GitHub repository.
- * @author Foothill College, Bita M
- *
- * Note: If you modified this file then update the author name with your: FirstName LastName
- * 
- * Reminder: Take a look at "Example Program Submission" in course Modules and follow the "Program Guidelines" 
- * to make sure your submission is complete.
+ * @author Foothill College, Swati Gupta
  */
 public class DemoGit 
 {
@@ -13,9 +9,9 @@ public class DemoGit
 	/**
 	 * Outputs my name
 	 */
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-		System.out.println("Hello. My name is [Enter your name here]");
+		System.out.println("Hello. My name is Swati");
 
 	}
 
